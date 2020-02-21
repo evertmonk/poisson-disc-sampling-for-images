c35989f373377425ea530fea38510a897234744e
Evert van der Horst <evert.van.der.horst@mediamonks.com>
2020-02-21 15:23:55 +0100

    feature(example): update example to show new images feature

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,44 @@
 import poissonImageSampler from './poissonImageSampler';
 
 function renderSamples(parent: any, grid: any) {
-  grid.forEach((sample: any) => parent.appendChild(createSampleElement(sample)));
+  grid.forEach((sample: any, i: number): void => parent.appendChild(createSampleElement(sample, i)));
 }
 
-function createSampleElement(sample: any) {
+function createSampleElement(sample: any, i: number) {
   const el = document.createElement('div');
+  const elSize = sample.radius * 2;
+
   el.classList.add('dot');
   el.style.left = sample.x + 'px';
   el.style.top = sample.y + 'px';
-  el.style.width = sample.radius * 2 + 'px';
-  el.style.height = sample.radius * 2 + 'px';
+  el.style.width = elSize + 'px';
+  el.style.height = elSize + 'px';
+
+  const sqEl = document.createElement('div');
+  const sqElSize = images[sample.index].size;
+
+  sqEl.classList.add('square');
+  sqEl.style.width = sqElSize + 'px';
+  sqEl.style.height = sqElSize + 'px';
+  sqEl.style.background = `url(https://picsum.photos/seed/${i * Math.random()}/${sqElSize}/${sqElSize}) center no-repeat`;
+
+  el.appendChild(sqEl);
 
   return el;
 }
 
 const parent = document.getElementById('root');
 
+const images = [
+  { size: 20 },
+  { size: 40 },
+  { size: 60 },
+  { size: 80 },
+  { size: 100 },
+];
+
 if (parent) {
   const { x, y, width, height } = parent.getBoundingClientRect();
-  const radius = 10;
-  const grid = poissonImageSampler({ bounds: { x, y, width, height }, radius, minDist: radius * 2.2, maxTries: 30 });
+  const grid = poissonImageSampler({ bounds: { x, y, width, height }, images, minDist: 10, maxTries: 30 });
   renderSamples(parent, grid);
 }
